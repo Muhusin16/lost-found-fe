14dbc2b10e26c028cc1c7d0cb9f31476e06d41a8
spriya0711 <priya@zool.in>
2024-09-03 15:30:10 +0530

    category and subcategory integration working

--- app/(admin)/admintool/page.tsx
+++ app/(admin)/admintool/page.tsx
@@ -8,6 +8,7 @@ const AdminTool = () => {
 
     const [category, setCategory] = useState([]);
     const [newCategory, setNewCategory] = useState('');
+    const [newSubCategory, setNewSubCategory] = useState('');
 
     const getCategories = async () => {
         console.log('categories')
@@ -21,8 +22,12 @@ const AdminTool = () => {
         }
     }
 
-    const handleInputChange = (e: any) => {
+    const handleInputChangeCategory = (e: any) => {
         setNewCategory(e.target.value);
+    }
+
+    const handleInputChangeSubCategory = (e: any) => {
+        setNewSubCategory(e.target.value);
     }
 
     const addCategory = async () => {
@@ -35,23 +40,41 @@ const AdminTool = () => {
         }
     }
 
+    const addSubCategory = async (category:any) => {
+        try {
+        const subCategory = category.sub_categories;     
+        const updatedSubCategory = [...subCategory, { id: subCategory.length+1, name:newSubCategory}]
+        
+        const response = await axiosInstance.put(`${apiUrls.categories}/${category._id}`, {sub_categories: updatedSubCategory})
+        console.log(response.data)
+        } catch (error) {
+            console.log(error);
+            
+        }
+    }
+
     useEffect(() => {
         getCategories();
         addCategory();
-    }, []);
+    }, [])
 
     return (
         <>
             <div>
-                <input onChange={handleInputChange} type="text" className='bg-gray-200' />
+                <input onChange={handleInputChangeCategory} type="text" className='bg-gray-200' />
                 <button onClick={() => addCategory()}>Add Category</button>
             </div>
             <div>
                 {category && category.map((each: any) => (<div>
                     <h1>{each.name}</h1>
+                    <div>
+                        <input onChange={handleInputChangeSubCategory} type="text" className='bg-gray-200' />
+                        <button onClick={() => addSubCategory(each)}>Add Sub Category</button>
+                    </div>
                     {each.sub_categories.map((subCategory: any) =>
                         <li>{subCategory.name}</li>)}
-                </div>))}
+                </div>
+                ))}
             </div>
 
         </>
